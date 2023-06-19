d0af681c9bf2cf6ac7ac78525780bd66e52a68eb
jtsang02 <jtsang02@student.ubc.ca>
2023-06-19 00:36:24 -0700

    modify require field

--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -13,14 +13,15 @@ export default function InputField ({ props }: {
 }) {
     return (
         <label className="block">
-            <span className="text-gray-700 font-medium px-1">{props.label}</span>
+            <span className="text-gray-700 font-medium px-1">{props.label}
+            {props.required? <span className="text-red-500">*</span> : null}
+            </span>
             <Input
                 className="mt-1 block w-full rounded-md p-1 bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
                 name={props.name}
                 id={props.id}
                 type={props.type}
                 placeholder={props.placeholder}
-                required={props.required}
                 onChange={props.onChange}
             />
         </label>
