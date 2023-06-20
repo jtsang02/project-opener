67b66fbf6ef8bec6b56152b0d7cc2a49e363ccbc
jtsang02 <jtsang02@student.ubc.ca>
2023-06-20 01:15:47 -0700

    Create Staff.ts

---
+++ src/Data/Staff.ts
@@ -0,0 +1,16 @@
+export const staff = [
+    { name: "Jeff Mitchell", initials: "JDM", role:"principal", email: 'jdm@ghl.ca' },
+    { name: "John Buscemi", initials: "JB", role:"principal", email: 'jb@ghl.ca' },
+    { name: "Jun Kim", initials: "JHK", role:"principal", email: 'jhk@ghl.ca'},
+    { name: "Adam Nadem", initials: "an", role:"principal", email:'an@ghl.ca'},
+
+    { name: "Darrell Li", initials: "DL", role:"associate", email:'dl@ghl.ca'},
+    { name: "Claire Yuan", initials: "CY", role:"associate", email:'cy@ghl.ca'},
+
+    { name: "Josiah Tsang", initials: "JT", role:"tech", email:'jt@ghl.ca'},
+    { name: "Morgan Trithart", initials: "MRT", role:"tech", email:'mrt@ghl.ca'},
+    { name: "Ruth Morrison", initials: "RM", role:"tech", email:'rm@ghl.ca'},
+
+    {name: "Kristina Lang", initials: "KL", role:"admin", email:'kl@ghl.ca'},
+    {name: "Cindy Yee", initials: "CYEE", role:"admin", email:'cyee@ghl.ca'},
+];
