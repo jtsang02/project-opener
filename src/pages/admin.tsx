4208be99380f17394f55a5f26277717025a90ed4
jtsang02 <jtsang02@student.ubc.ca>
2023-06-27 01:58:27 -0700

    start table of projects

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,16 +1,16 @@
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import Header from "@/Components/Header";
+import Project from "@/Models/Project";
 
 export default function Form() {
 
     //call the api
-    const [data, setData] = useState<any>(null);
+    const [projects, setProjects] = useState<Project[]>([]);
 
     useEffect(() => {
         fetch('/api/projects')
             .then(res => res.json())
-            .then(data => setData(data));
+            .then(projects => setProjects(projects));
     }, []);
 
     return (
@@ -21,9 +21,62 @@ export default function Form() {
                 linkurl: "/",
                 linkName: "Return to Form"
             }} />
-            admin page ...            
-            <p>{data?.name}</p>
 
+            <div className="flex flex-col">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Project Name
+                                        </th>
+                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Project Address
+                                        </th>
+                                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Principal
+                                        </th>
+                                        <th scope="col" className="relative px-6 py-3">
+                                            <span className="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                    {projects.map((project) => (
+                                        <tr key={project.name}>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <div className="flex items-center">
+                                                    <div className="ml-4">
+                                                        <div className="text-sm font-medium text-gray-900">
+                                                            {project.name}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+
+                                                <div className="text-sm text-gray-900">{project.address}</div>
+
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                    {project.internalContact.principal}
+                                                </span>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    );
+    )
 }
