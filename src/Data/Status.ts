b0256439fc4d9db13c0b35d5f248925e09609ba4
jtsang02 <jtsang02@student.ubc.ca>
2023-07-05 01:27:05 -0700

    Update Status.ts

--- src/Data/Status.ts
+++ src/Data/Status.ts
@@ -1,5 +1,5 @@
-export const status: string[] = [
-    'Open',
-    'Pending',
-    'Closed'
-    ];
+export const statuses = [
+    { label: "Open", value: "Open", color: "#4ade80", textColor: "#14532d" },
+    { label: "Pending", value: "Pending", color: "#fb923c", textColor: "#78350f" },
+    { label: "Closed", value: "Closed", color: "#f87171", textColor: "#7f1d1d" },
+];
