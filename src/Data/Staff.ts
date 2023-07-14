2e8a9ae259cc530a6a89ee83158991c2d37458ba
jtsang02 <jtsang02@student.ubc.ca>
2023-07-14 02:56:31 -0700

    remove live emails for testing

--- src/Data/Staff.ts
+++ src/Data/Staff.ts
@@ -1,19 +1,22 @@
 import Staff from '../Models/Staff';
 
-export const staff: Staff[]  = [
-    { name: "Jeff Mitchell", initials: "JDM", role:"principal", email: 'jdm@ghl.ca' },
-    { name: "John Buscemi", initials: "JB", role:"principal", email: 'jb@ghl.ca' },
-    { name: "Jun Kim", initials: "JHK", role:"principal", email: 'jhk@ghl.ca'},
-    { name: "Adam Nadem", initials: "AN", role:"principal", email:'an@ghl.ca'},
-
-    { name: "Darrell Li", initials: "DL", role:"associate", email:'dl@ghl.ca'},
-    { name: "Claire Yuan", initials: "CY", role:"associate", email:'cy@ghl.ca'},
-
+const staff: Staff[]  = [
+    // principals
+    { name: "Jeff Mitchell", initials: "JDM", role:"principal", email: 'jt@ghl.ca' },
+    { name: "John Buscemi", initials: "JB", role:"principal", email: 'jt@ghl.ca' },
+    { name: "Jun Kim", initials: "JHK", role:"principal", email: 'jt@ghl.ca'},
+    { name: "Adam Nadem", initials: "AN", role:"principal", email:'jtstripes95@gmail.com'},     // test email
+    // associates
+    { name: "Darrell Li", initials: "DL", role:"associate", email:'jt@ghl.ca'},
+    { name: "Claire Yuan", initials: "CY", role:"associate", email:'jt@ghl.ca'},
+    // tech staff
     { name: "Josiah Tsang", initials: "JT", role:"tech", email:'jt@ghl.ca'},
-    { name: "Morgan Trithart", initials: "MRT", role:"tech", email:'mrt@ghl.ca'},
-    { name: "Ruth Morrison", initials: "RM", role:"tech", email:'rm@ghl.ca'},
-
-    {name: "Kristina Lang", initials: "KL", role:"admin", email:'kl@ghl.ca'},
-    {name: "Cindy Yee", initials: "CYEE", role:"admin", email:'cyee@ghl.ca'},
+    { name: "Morgan Trithart", initials: "MRT", role:"tech", email:'jtstripes95@gmail.com'},  // test email
+    { name: "Ruth Morrison", initials: "RM", role:"tech", email:'jt@ghl.ca'},
+    // admin
+    {name: "Kristina Lang", initials: "KL", role:"admin", email:'jt@ghl.ca'},
+    {name: "Cindy Yee", initials: "CYEE", role:"admin", email:'jt@ghl.ca'},
     
 ];
+
+export default staff;
