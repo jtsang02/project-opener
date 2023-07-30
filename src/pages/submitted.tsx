d6535a2467bffabbbced6a14495efbb77f6121ea
jtsang02 <jtsang02@student.ubc.ca>
2023-07-29 20:03:15 -0700

    Update submitted.tsx

--- src/pages/submitted.tsx
+++ src/pages/submitted.tsx
@@ -6,12 +6,13 @@ export default function Submitted () {
         <>
             <div className="min-h-screen bg-gray-100 py-6 flex flex-col sm:py-12">
                 <div className="flex justify-center mx-auto px-4 py-8 ">
-                    <div className="">
-                        <h1 className="text-2xl font-bold mb-4 text-center text-green-600">Submitted!</h1>
+                    <div className="text-center">
+                        <h1 className="text-2xl font-bold mb-4 text-green-600">Submitted!</h1>
                         <p className="text-xl font-bold ">Thank you for submitting your project request.</p>
+                        <p className="text-m font-md mt-5">Admin will contact you soon with a project number.</p>
                     </div>
                 </div>
-                <div className="flex justify-center mx-auto px-4 py-8 ">
+                <div className="flex justify-center mx-auto px-4">
                     <div className="mt-1 text-m font-medium bg-gray-300 rounded-lg py-1 px-2 text-red-800 hover:text-red-900 hover:bg-gray-400">
                         <Link href="/">
                             <a>Return to Form</a>
