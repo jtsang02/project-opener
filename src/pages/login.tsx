01b43dc30e1b8eefc80ce874a3c9327da09c1774
jtsang02 <jtsang02@student.ubc.ca>
2023-08-03 00:40:22 -0700

    test signOut function in next-auth

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { signOut } from "next-auth/react";
 
-export default function Login () {
-
+export default function Login() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  // each time the page loads, check if the user is logged in
+  // if they are logged in, log them out
+  // this is to prevent the user from being logged in on multiple devices
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Perform login logic here, e.g., submit data to the server for authentication
-    
+
     try {
       const res = await signIn("credentials", {
         email,
@@ -26,12 +30,10 @@ export default function Login () {
       } else {
         alert(res.error);
       }
-    }
-    catch (error) {
+    } catch (error) {
       console.error(error);
     }
   };
-
 
   const handleClear = () => {
     setEmail("");
@@ -40,7 +42,10 @@ export default function Login () {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="flex flex-col p-8 bg-gray-100 shadow rounded-2xl w-5/6 max-w-md">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col p-8 bg-gray-100 shadow rounded-2xl w-5/6 max-w-md"
+      >
         <h2 className="text-2xl font-bold mb-4 text-center">Admin Login</h2>
         <label className="mb-2 flex flex-col">
           Email:
@@ -63,22 +68,35 @@ export default function Login () {
           />
         </label>
         <div className="flex justify-center space-x-4">
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-2xl hover:bg-blue-700">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded-2xl hover:bg-blue-700"
+          >
             Submit
           </button>
-          <button type="button" onClick={handleClear} className="bg-gray-500 text-white px-4 py-2 rounded-2xl hover:bg-gray-700">
+          <button
+            type="button"
+            onClick={handleClear}
+            className="bg-gray-500 text-white px-4 py-2 rounded-2xl hover:bg-gray-700"
+          >
             Clear
           </button>
         </div>
         <div className="flex justify-between">
-            <button className="mt-1 text-m font-medium py-1 px-2 text-blue-800 hover:text-blue-600">
-                <Link href="/">Form</Link>
-            </button>
-            <button className="mt-1 text-m font-medium py-1 px-2 text-blue-800 hover:text-blue-600">
-                <Link href="/register">Register</Link>
-            </button>
+          <button className="mt-1 text-m font-medium py-1 px-2 text-blue-800 hover:text-blue-600">
+            <Link href="/">Form</Link>
+          </button>
+          <button className="mt-1 text-m font-medium py-1 px-2 text-blue-800 hover:text-blue-600">
+            <Link href="/register">Register</Link>
+          </button>
+          {/* <button
+            className="mt-1 text-m font-medium py-1 px-2 text-blue-800 hover:text-blue-600"
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </button> */}
         </div>
       </form>
     </div>
   );
-};
+}
