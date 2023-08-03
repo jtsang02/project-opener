b9367d6e8c57dd5aa87d07bb7d44a40dbb28a7d9
jtsang02 <jtsang02@student.ubc.ca>
2023-08-03 00:26:39 -0700

    update compareDates

--- src/Utils/dates.tsx
+++ src/Utils/dates.tsx
@@ -6,10 +6,7 @@ function formatDate(dateString: string | Date) {
 
 function compareDates(date1: Date, date2: Date) {
     // 2023-06-26T23:00:49+9:00 -> 2023-06-26
-    const date1Str = formatDate(date1);
-    const date2Str = formatDate(date2);
-    // compoare if date1 is greater than date2
-    return date1Str.localeCompare(date2Str) > 0;
+    return moment(date1).isAfter(date2);
 }
 
 
